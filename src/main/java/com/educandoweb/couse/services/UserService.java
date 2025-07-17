ad1c3977b305dfd1932cb3899c40cd27bb91ac29
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-07-17 19:29:33 -0300

    User update

--- src/main/java/com/educandoweb/couse/services/UserService.java
+++ src/main/java/com/educandoweb/couse/services/UserService.java
@@ -33,5 +33,18 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id); //esse metodo "getReferenceById" é mais vantagoso que o "findById(id)" porque é um objeto monitorado sendo possivel trabalhar com ele, o "findById(id)" custaria o acesso ao banco para buscar o usuario. 
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
 
+	//nesse metodo auxiliar nem todos os dados necessariamente devem ser atualizar, no exemplo atualizamos somente 3 dados. o id nem a senha serão atualizados nesse caso
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
 }
